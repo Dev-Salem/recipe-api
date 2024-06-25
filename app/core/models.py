e0ecb548ba514e9df45603cb71fe631c3210468b
Dev Salem <dev.salemsaleh@gmail.com>
2024-06-25 08:22:10 +0300

    feat: Add email normalization and create_superuser method to UserManager. Add tests for create_superuser and create_user

--- app/core/models.py
+++ app/core/models.py
@@ -6,8 +6,18 @@ class UserManager(BaseUserManager):
     '''Manager for users'''
     def create_user(self, email, password=None,**extra_fields):
         '''Create a new user and return it'''
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        '''Create a new superuser and return it'''
+        user = self.create_user(email, password=password)
+        user.is_staff =True
+        user.is_superuser =True
         user.save(using=self._db)
         return user
 
