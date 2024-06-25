ecede7fd783d95e02d65bf84eca3b3ca7c0c4b31
Dev Salem <dev.salemsaleh@gmail.com>
2024-06-25 09:45:40 +0300

    chore: Add rest_framework and drf_spectacular to INSTALLED_APPS and update requirements

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local app
     "core.apps.CoreConfig"
+    # 3rd party
+    "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -126,4 +130,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = 'core.User'
+AUTH_USER_MODEL = "core.User"
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEME_CLASS": "drf_spectacular.openapi.AutoSchema"
+}
